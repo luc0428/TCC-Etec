ca54fd623313f91e90998fe6206af929fb287d3b
luc0428 <125515363+luc0428@users.noreply.github.com>
2023-09-15 21:40:10 -0300

    código principal

---
+++ TCC/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body background="imagens/back.jpg" > 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+		   
+	<nav class="navbar fixed-top navbar-Warning bg-Warning navbar-expand-lg"> 
+	<!--<nav class="navbar fixed-top navbar-primary navbar-expand-lg" style="background-color:#8B0000-->
+
+				<a href="index.php" class="navbar-brand">
+					<img src="img/logoloja.png"  width="160" height="150" alt="">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav1" >
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-between" id="nav1">
+					<ul class="navbar-nav">
+						<li class="nav-item ">
+							<a href="loja.php" class="nav-link">loja</a>
+						</li>
+						<li class="nav-item">
+							<a href="sobre.php" class="nav-link">sobre Wanel Acessórios</a>
+						</li>
+						<li class="nav-item">
+							<a href="produtos.php" class="nav-link">Produtos</a>
+						</li>
+																							
+					</ul>
+				</div>
+			</nav>
+		</div>					
+	</div>
+       
+           
+</body>
+</html>
